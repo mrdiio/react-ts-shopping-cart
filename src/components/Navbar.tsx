7ca386d8f07ef13a9d093cc21bc96c489eff5f75
Dio <dionatsir@gmail.com>
2023-01-24 17:59:34 +0700

    belajar react-tss

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+
+export const Navbar = () => {
+  const { openCart, cartQuantity } = useShoppingCart();
+
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/store">
+            Store
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/about">
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
+          <Button
+            style={{ width: '3rem', height: '3rem', position: 'relative' }}
+            variant="outline-primary"
+            className="rounded-circle"
+            onClick={openCart}
+          >
+            <svg
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+            >
+              <path d="M13 17a2 2 0 1 0 3.999.001A2 2 0 0 0 13 17zM3 17a2 2 0 1 0 4 0 2 2 0 0 0-4 0zm3.547-4.828L17.615 9.01A.564.564 0 0 0 18 8.5V3H4V1.4c0-.22-.181-.4-.399-.4H.399A.401.401 0 0 0 0 1.4V3h2l1.91 8.957.09.943v1.649c0 .219.18.4.4.4h13.2c.22 0 .4-.182.4-.4V13H6.752c-1.15 0-1.174-.551-.205-.828z" />
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: 'white',
+                width: '1.5rem',
+                height: '1.5rem',
+                position: 'absolute',
+                bottom: '0',
+                right: '0',
+                transform: 'translate(25%, 25%)',
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
+      </Container>
+    </NavbarBs>
+  );
+};
